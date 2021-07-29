06a5ad56f101edf8e9c16a7f0cfd6656673ca7a6
AlinaWoellner <82510384+AlinaWoellner@users.noreply.github.com>
2021-07-29 20:01:45 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
-#include <FastLED.h>
+#include <FastLED.h> // FastLED library einbinden
 
-#if FASTLED_VERSION < 3001000
+#if FASTLED_VERSION < 3001000 // Prüfen, ob die richtige FastLED Version vorhanden ist
 #error "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
 #define DATA_PIN    3 // Pin, an dem die LEDs angeschlossen sind
 #define LED_TYPE    WS2812  // LED Typ angeben
 #define COLOR_ORDER GRB
-#define NUM_LEDS    12  // So viele LEDs hat der Streifen
+#define NUM_LEDS    12  // So viele LEDs hat der Ring/ Streifen/ etc.
 CRGB leds[NUM_LEDS];  // Array von FastLED Pixel RGB-Werten
 
 #define BRIGHTNESS          96   
-#define FRAMES_PER_SECOND  120  
+#define FRAMES_PER_SECOND  120   
 
-// Damit der Code funktioniert, müssen die später aufgerufenen Muster noch vor der setup() Funktion deklariert werden
+// Damit der Code funktioniert, müssen die später aufgerufenen pattern noch vor der setup() Funktion deklariert werden
 void rainbow();
 void rainbowWithGlitter(); 
 void addGlitter( fract8 chanceOfGlitter);
@@ -29,25 +29,24 @@ void setup() {  // Diese Funktion wird bei Programmstart einmal aufgerufen
   delay(3000); // 3 Sekunden Verzögerung
   
   // FastLED sagen, wie die Konfiguration des LED-Rings ist
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // Library um LEDs zu kontrollieren; WS2812 Leds besitzt einen integrierten Controller, die über einen Arduino angesteuert werden können
   //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
-  // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 
 }
 
 
-// Liste der durchlaufenden Muster. Jedes Muster wird weiter unten definiert. 
+// Liste der durchlaufenden pattern. Alle pattern werden weiter unten definiert 
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
-uint8_t gCurrentPatternNumber = 0; // Index Nummer des aktuellen Musters 
-uint8_t gHue = 0; // Wechselnde "Grundfarbe", von vielen Mustern benutzt 
+uint8_t gCurrentPatternNumber = 0; // Index Nummer des aktuellen pattern 
+uint8_t gHue = 0; // Wechselnde "Grundfarbe", von vielen pattern benutzt 
   
-void loop()
+void loop() // Ausführung des Programms
 {
-  // Das aktuelle Muster einmal aufrufen und das "leds" Array updaten 
+  // Das aktuelle pattern einmal aufrufen und das "leds" Array updaten 
   gPatterns[gCurrentPatternNumber]();
 
 
@@ -55,20 +54,19 @@ void loop()
   
   FastLED.delay(1000/FRAMES_PER_SECOND); // Verlangsamung der Animation
 
-  // Regelmäßige Aktualisierungen
+  // Regelmäßige updates
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // langsam die "Grundfarbe" durch die Farben des Regenbogens führen
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // In regelmäßigen Abständen das Muster wechseln
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // In regelmäßigen Abständen das pattern wechseln
 }
 
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0])) 
 
 void nextPattern()
 {
-  // Zur aktuellen Musternummer eine Ziffer hinzufügen und am Ende umbrechen  and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
 
-// Die einzelnen Muster
+// Die einzelnen pattern:
 void rainbow() 
 {
   // FastLEDs eingebauter Regenbogengenerator
@@ -99,7 +97,7 @@ void confetti()
 
 void sinelon()
 {
-  // Ein farbiger Punkt, der hin und her bewegt, mit verblassenden Spuren
+  // Ein farbiger Punkt, der sich hin und her bewegt, mit verblassenden Spuren
   fadeToBlackBy( leds, NUM_LEDS, 20);
   int pos = beatsin16(13,0,NUM_LEDS);
   leds[pos] += CHSV( gHue, 255, 192);
@@ -117,7 +115,7 @@ void bpm()
 }
 
 void juggle() {
-  // Zcht Punkte synchronisieren sich auf eine Farbe
+  // Acht Punkte synchronisieren sich auf eine Farbe
   fadeToBlackBy( leds, NUM_LEDS, 20);
   byte dothue = 0;
   for( int i = 0; i < 8; i++) {
